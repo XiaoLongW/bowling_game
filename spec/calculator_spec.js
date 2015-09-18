33cce4f7aab9696b4c1a832b46f3a30a8779d084
XiaoLongW <WangXiaoLong2016@outlook.com>
2015-09-18 14:51:29 +0800

    add calculator_spec.js

---
+++ spec/calculator_spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var Calculator = require('../src/model/calculator');
+
+describe('Calculator', function(){
+	var calculator;
+	beforeEach(function () {
+		calculator = new Calculator();
+	});
+
+  it('should have allFrames and bonus', function(){
+		expect(calculator.allFrames).toEqual([]);
+		expect(calculator.bonus).toEqual('');
+	});
+
+	describe('#scan()', function () {
+		it('should scan right', function () {
+			var input = 'X|7/|9-|X|-8|8/|-6|X|X|X||81';
+			calculator.scan(input);
+
+			expect(calculator.allFrames).toEqual([ 'X', '7/', '9-', 'X', '-8', '8/', '-6', 'X', 'X', 'X' ]);
+			expect(calculator.bonus).toEqual('81');
+		});
+	});
+});
